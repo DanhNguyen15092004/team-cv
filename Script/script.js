2a1c42365a9c658fe3cb64e68b58ea7e86236424
WannaSu1cide <ngdanh1509@gmail.com>
2024-04-05 00:04:16 +0700

    Make some CSS

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,19 @@
 // script.js
 
 // Chọn icon bằng id
-const icon = document.getElementById("icon");
+const icons = document.querySelectorAll(".icon");
+var urls =  [
+    "https://www.facebook.com/?stype=lo&deoia=1&jlou=AfdGa4vt8XNJn0VL4wWk2IZ0il-I1CYwsUGEvaraxxvwKRB_nGDn5x7JpGWGbgoMINa65nMWr7bXGBMJol3xqaWdTrGs7JAVDpVD5t-s9SjoCg&smuh=17326&lh=Ac-10piyxGe7sZEb3p8", 
+    "https://www.messenger.com/", 
+    "https://www.instagram.com/",
+    "https://twitter.com/home",
+    "https://www.linkedin.com/login"
+]
+icons.forEach((icon,e) => { 
+        icon.addEventListener("click" , () => {
+            const clickedIndex = Array.from(icons).indexOf(icon);
 
-// Thêm sự kiện 'click' cho icon
-icon.addEventListener("click", () => {
-    // Chuyển hướng đến trang web mong muốn, trong trường hợp này là "https://example.com"
-    console.log(123)
-    window.location.href = "https://cdnjs.com/libraries/font-awesome";
+            window.location.href = urls[clickedIndex];
+        })
 });
+
